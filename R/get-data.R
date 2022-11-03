44aa5608a4a9b387c0b650495c28ec1a8a4461f9
Joshua Lorin <jlorin@gavi.org>
2022-11-04 00:07:53 +0100

    adding more get data functions

--- R/get-data.R
+++ R/get-data.R
@@ -35,6 +35,7 @@ set_root <- function(){
 #' get_groupings("2022-07")
 #' # can also save as object to re-use
 #' groupings <- get_groupings("2022-07")
+#'
 
 get_groupings <- function(version_date){
 
@@ -66,3 +67,132 @@ get_groupings <- function(version_date){
   }
 
 }
+
+
+#' Retrieve WUENIC data
+#'
+#' Retrieves WUENIC data from the shared drive. NOTE: Only works for 2021 or later versions.
+#'
+#' @param version_date A string in the format of "yyyy-mm", corresponding to the version of the WUENIC file you wish to load.
+#' @param format A string used to determine which format of WUENIC file to load. Options are: "normal", "old_new", and "to_share".
+#'
+#' @return a WUENIC data frame
+#' @export
+#'
+#' @examples
+#'
+#' get_wuenic("2022-07")
+#' get_wuenic("2021-07", "old_new")
+
+get_wuenic <- function(version_date, format = "normal"){
+
+  root <- set_root()
+  version_year <- stringr::str_sub(version_date, end = 4)
+
+  if(format == "normal"){format <- "_"}else{format <- paste0("_", format, "_")}
+
+  path <- file.path(root, "CPMM", "Datasets", "Coverage", "WUENIC", version_year, "outputs", paste0("wuenic", format, version_date, ".rds"))
+  msg <- paste0("Reading in WUENIC file dated: ", version_date)
+
+  message(msg)
+  readRDS(path)
+
+}
+
+#' Retreive dates of JRF files
+#'
+#' This is a helper function to look at dates for the JRF files.
+#'
+#' @param source A string used to determine which format of JRF file to load. Options are: "admin" (default), "official", "system.", and "subnational".
+#' @param latest Use TRUE if you only want latest file date.
+#' @param guids Use TRUE if you want a file with GUIDs. Only relevant for subnational data.
+#'
+#' @return A vector of dates.
+#' @export
+#'
+#' @examples
+#'
+#' get_jrf_dates()
+#' get_jrf_dates("official")
+#' get_jrf_dates("subnational", guids = TRUE)
+#'
+#' @importFrom rlang .data
+
+get_jrf_dates <- function(source = "admin", latest = FALSE, guids = FALSE){
+
+  root <- set_root()
+  folders <- list.files(file.path(root, "CPMM", "Datasets", "Coverage", "JRF", stringr::str_to_title(source)), pattern = "^20")
+  path_years <- folders[5:length(folders)]
+  paths <- file.path(root, "CPMM", "Datasets", "Coverage", "JRF", stringr::str_to_title(source), path_years, "outputs")
+
+  files <- unlist(lapply(paths, list.files))
+  stringr::str_length(files)
+
+  if(source == "admin"){
+    name_length <- 24
+  }else if(source == "official"){
+    name_length <- 27
+  }else if(source == "system"){
+    name_length <- 21
+  }else if(source == "subnational" & guids == F){
+    name_length <- 30
+  }else if(source == "subnational" & guids == T){
+    name_length <- 36
+  }
+
+  dates <- dplyr::as_tibble(files) %>%
+    dplyr::filter(stringr::str_detect(.data$value, ".rds")) %>%
+    dplyr::filter(stringr::str_length(.data$value) == name_length) %>%
+    dplyr::mutate(value = stringr::str_sub(.data$value, -14,-5)) %>%
+    dplyr::pull()
+
+  if(latest == T){max(dates)}else{dates}
+
+
+}
+
+
+
+#' Retrieve JRF data
+#'
+#' Retrieves JRF data from the shared drive. NOTE: Only works for 2021 or later versions.
+#'
+#' @param version_date A string in the format of "yyyy-mm-dd", corresponding to the version of the JRF file you wish to load.
+#' @param source A string used to determine which format of JRF file to load. Options are: "admin" (default), "official", "system.", and "subnational".
+#' @param guids Use TRUE if you want a file with GUIDs. Only relevant for subnational data.
+#'
+#' @return a JRF data frame.
+#' @export
+#'
+#' @examples
+#'
+#' get_jrf("2022-08-03")
+#' get_jrf("2022-09-02", "subnational", guids = TRUE)
+#' # to dynamically always pull most recent file
+#' get_jrf(get_jrf_dates("official", latest = TRUE), "official")
+
+get_jrf <- function(version_date, source = "admin", guids = FALSE){
+
+  root <- set_root()
+  version_year <- stringr::str_sub(version_date, end = 4)
+
+  if(source=="admin"){
+    filename <- paste0("jrf_admin_", version_date, ".rds")
+  }else if(source == "official"){
+    filename <- paste0("official_jrf_", version_date, ".rds")
+  }else if(source == "system"){
+    filename <- paste0("system_", version_date, ".rds")
+  }else if(source == "subnational"){
+    filename <- paste0("jrf_subnational_", version_date, ".rds")
+  }
+
+  if(guids==TRUE){filename <- paste0("jrf_subnational_GUIDS_", version_date, ".rds")}
+
+  path <- file.path(root, "CPMM", "Datasets", "Coverage", "JRF", stringr::str_to_title(source), version_year, "outputs", filename)
+  msg <- paste0("Reading in JRF", stringr::str_to_title(source), "file dated: ", version_date)
+
+  message(msg)
+  readRDS(path)
+
+}
+
